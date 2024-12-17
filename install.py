01fbb7722e6ccc63a5c5be254ef38def744f2424
SURAM SUMANTH <165796407+Sumanth8913@users.noreply.github.com>
2024-12-17 09:51:32 +0530

    Add files via upload
    This project creates a chatbot that answers questions based on content from uploaded PDFs using the RAG (Retrieval-Augmented Generation) pipeline. It extracts text from PDFs, splits it into chunks, and stores it in a FAISS vector database for efficient querying. The system uses LangChain and a Groq-powered LLM to generate contextually relevant answers from the extracted data.
    
    Features:
    
    Extract text from PDFs using pdfminer.six.
    Split text into chunks and embed using HuggingFaceEmbeddings.
    Store embeddings in a FAISS vector database for fast retrieval.
    Use a pre-trained LLM for answering user queries.
    Installation: Install necessary libraries using pip install pdfminer.six streamlit pickle5 langchain langchain-groq faiss-cpu huggingface_hub.
    
    Upload PDFs, ask questions, and get answers from the extracted content.

---
+++ install.py
@@ -0,0 +1,9 @@
+Python 3.12.0 (tags/v3.12.0:0fb18b0, Oct  2 2023, 13:03:39) [MSC v.1935 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> !pip install pdfminer.six
+... !pip install streamlit
+... !pip install pickle5
+... !pip install langchain
+... !pip install langchain-groq
+... !pip install -U langchain-community
+... !pip install faiss-cpu
